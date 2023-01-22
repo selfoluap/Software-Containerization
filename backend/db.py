5e7eb4f87ebaf1850e1a3fd3eec44a1570e2dffb
GSav <43476526+0chonko@users.noreply.github.com>
2023-01-22 22:55:08 +0100

    first steps in the db stuff

---
+++ backend/db.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+
+def print_tasks_day():
+ 
+def print_tasks_week():
+  
+def create_task(taskID, taskDate, startingTime, endingTime):
+  
+def update_task(taskID, taskDate, startingTime, endingTime):
+  
+def remove_task(taskID, taskDate, startingTime):
+  
