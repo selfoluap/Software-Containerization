82f79d7a57e41648212bc25a7479be5188d1d59a
0chonko <german.savchenko@gmail.com>
2023-01-26 20:50:12 +0100

    working_api_db

--- backend/server/db.py
+++ backend/server/db.py
@@ -1,12 +1,63 @@
+import os
 import sqlalchemy
+from dotenv import load_dotenv, find_dotenv
 
-def print_tasks_day():
- 
-def print_tasks_week():
-  
-def create_task(taskID, taskDate, startingTime, endingTime):
-  
-def update_task(taskID, taskDate, startingTime, endingTime):
-  
-def remove_task(taskID, taskDate, startingTime):
-  
+load_dotenv(find_dotenv())
+
+engine = sqlalchemy.create_engine(os.environ.get('DB_URL'))
+metadata = sqlalchemy.MetaData()
+
+# # init psycopg2
+# # conn = psycopg2.connect(
+# #     database = os.environ.get("POSTGRES_DB"),
+# #     user=os.environ.get("POSTGRES_USER"),
+# #     password=os.environ.get("POSTGRES_PASSWORD"),
+# #     host="localhost"
+# # )
+
+conn = engine.connect()
+tasksTable = sqlalchemy.Table('tasksTable', metadata,
+                              sqlalchemy.Column('taskID', sqlalchemy.String),
+                              sqlalchemy.Column('timeStart', sqlalchemy.Time),
+                              sqlalchemy.Column('timeEnd', sqlalchemy.Time),
+                              sqlalchemy.Column('date', sqlalchemy.Date),
+                              )
+
+
+def init():
+    exist = sqlalchemy.inspect(engine).has_table("tasksTable")
+    if exist == False:
+        print("=Table doesn't exist=")
+        metadata.create_all(engine)
+        print("=Table was created=")
+        return ("<p> done boy</p>")
+    else:
+        return ("<p>data up to date</p>")
+
+# def print_tasks_week():
+#   return 0
+
+
+def create_task(_taskID, _taskDate, _startingTime, _endingTime):
+    insrt = (
+        tasksTable.insert().
+        values(taskID=_taskID, date=_taskDate,
+               timeStart=_startingTime, timeEnd=_endingTime)
+    )
+    conn.execute(insrt)
+    return ("<p>inserted successfully</p>")
+
+
+def update_task(taskID_old, taskID_new, taskDate_new, startingTime_new, endingTime_new):
+    stmt = tasksTable.update().where(tasksTable.c.taskID == taskID_old).values(
+        taskID=taskID_new, date=taskDate_new, timeStart=startingTime_new,
+        timeEnd=endingTime_new
+        )
+    conn.execute(stmt)
+    return ("<p> task updated</p>")
+
+
+def remove_task(_taskID):
+    stmt = tasksTable.delete().where(tasksTable.c.taskID == _taskID)
+    conn.execute(stmt)
+    return ("<p>removed successfully</p>")
--- backend/server/server.py
+++ backend/server/server.py
@@ -1,64 +1,84 @@
-#!flask/bin/python
-import flask
-import psycopg2
-import sqlalchemy
-from db import print_tasks_day, print_tasks_week, update_task, remove_task, create_task
-from dotenv import load_dotenv
+# #!flask/bin/python
+from flask import *
+# import os
+# from db import print_tasks_day, print_tasks_week, update_task, remove_task, create_task, init
+from db import init, create_task, update_task, remove_task
+# import sqlalchemy
+from dotenv import load_dotenv, find_dotenv
+import datetime
+
+
+
+# load_dotenv(find_dotenv())
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/'
-
-# conn = psycopg2.connect(
-#     database = "",
-#     user="",
-#     password="",
-#     host="0.0.0.0"
-# )
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+# db = sqlalchemy(app)
 
 
+
+
+# class Item(db.Model):
+#     __tablename__='tasks'
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(80), unique=True, nullable=False)
+#     content = db.Column(db.String(120), unique=True, nullable=False)
+
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+#
 @app.route("/")
-def init():
-    return "<p>Good morning beatiful. It is time to get things done hehe </p>"
+def initialize():
+    return init();
 
-@app.route("/db/tasks/day", methods=['GET'])
-def print_tasks_day():
-    return jsonify({'tasks': get_day_tasks()})
+# @app.route('/', methods=['POST'])
+# def my_form_post():
+#     taskID = request.form['taskID']
+#     taskDate = request.form['taskDate']
+#     startingTime = request.form['startingTime']
+#     endingTime = request.form['endingTime']
+#     # create_task(taskID, taskDate, startingTime, endingTime)
+#     return "<p> New task! Keep it up. </p>"
+# @app.route("/db/tasks/day", methods=['GET'])
+# def print_tasks_day():
+#     return jsonify({'tasks': get_day_tasks()})
 
-@app.route("/db/tasks/week", methods=['GET'])
-def print_tasks_week():
-    return jsonify({'tasks': get_week_tasks()})
+# @app.route("/db/tasks/week", methods=['GET'])
+# def print_tasks_week():
+#     return jsonify({'tasks': get_week_tasks()})
 
-@app.route("/db/task/create", methods=['POST'])
-def create_task():
-    input = request.get_json() #get input for the task..
-    taskID = input['taskID']
-    taskDate = input['taskDate']
-    startingTime = input['timeStart']
-    endingTime = input['timeEnd']
-    create_task(taskID, taskDate, startingTime, endingTime)
-    return "<p> New task! Keep it up. </p>"
+@app.route("/server/createTask", methods=['GET','POST'])
+def create_task_serv():
+    date = datetime.date(2023,5,8)
+    timeStart = datetime.time(hour=20, minute=30)
+    timeEnd = datetime.time(hour=23, minute=30)
+    return create_task('papaya', date, timeStart, timeEnd)
 
 
-@app.route("/task/update", methods=['POST'])
-def update_task():
-    input = request.get_json() #get input for the task
-    taskID = input['taskID']
-    taskDate = input['taskDate']
-    startingTime = input['timeStart']
-    endingTime = input['timeEnd']
-    update_task(taskID, taskDate, startingTime, endingTime)
-    return "<p> Updated. </p>"
+@app.route("/server/updateTask", methods=['GET'])
+def update_task_serv():
+#     input = request.get_json() #get input for the task
+#     taskID = input['taskID']
+#     taskDate = input['taskDate']
+#     startingTime = input['timeStart']
+#     endingTime = input['timeEnd']
+    date = datetime.date(2050,5,8)
+    timeStart = datetime.time(hour=20, minute=30)
+    timeEnd = datetime.time(hour=23, minute=30)
+    return update_task("papaya", "papaya_new", date, timeStart, timeEnd)
+    
 
-@app.route("/task/remove", methods=['GET'])
-def remove_task():
-    input = request.get_json() #get input for the task
-    taskID = input['taskID']
-    taskDate = input['taskDate']
-    startingTime = input['timeStart']
-    remove_task(taskID, taskDate, startingTime) #assume there cannot be 2 tasks with the same name in parallel
+@app.route("/server/removeTask", methods=['GET'])
+def remove_task_serv():
+    # input = request.get_json() #get input for the task
+    # taskID = input['taskID']
+    # taskDate = input['taskDate']
+    # startingTime = input['timeStart']
+    remove_task("papaya_new") #assume there cannot be 2 tasks with the same name in parallel
     return "<p>Removed.</p>"
 
-
-
+ 
 if __name__ == '__main__': 
     app.run(debug=True) 
