8bbdc852e36585e5f80e290bc7a456a53852e0b9
GSav <43476526+0chonko@users.noreply.github.com>
2023-01-30 17:11:53 +0100

    Update server.py

--- backend/server/server.py
+++ backend/server/server.py
@@ -2,14 +2,19 @@
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-# from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv
+from flask_cors import CORS, cross_origin
 
-# load_dotenv(find_dotenv())
+
+load_dotenv(find_dotenv())
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 db.init_app(app)
+cors = CORS(app, origins="*")
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 class Todo(db.Model):
   __tablename__ = 'TodoTable'
@@ -21,27 +26,30 @@ class Todo(db.Model):
     self.title = title
     self.description = description
 
-db.create_all()
 
-@app.route('/server/todos', methods=['GET'])
-@cross_origin() 
-def get_todos(): 
-    todos = TodoTable.query.all()
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/server/get_todos', methods=['GET'])
+@cross_origin()
+def get_todos():
+    todos = Todo.query.all()
     return jsonify(todos)
 
 
-@app.route("/server/todos", methods=['POST'])
+@app.route("/server/create_todos", methods=['POST'])
 @cross_origin()
-def create_todo():
+def create_todo_server():
     body = request.get_json()
     db.session.add(Todo(body['title'], body['description']))
     db.session.commit()
     return "todo successfully created"
 
     
-@app.route("/server/todos/<id>", methods=['DELETE'])
+@app.route("/server/delete_todos/<id>", methods=['DELETE'])
 @cross_origin()
-def delete_todo(id):
+def remove_todo_server(id):
     db.session.query(Todo).filter_by(id=id).delete()
     db.session.commit()
     return "todo successfully deleted"
@@ -49,3 +57,4 @@ def delete_todo(id):
  
 if __name__ == '__main__': 
     app.run(debug=True) 
+ 
