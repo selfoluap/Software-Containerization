4b125c7d929e6a3f89c1ad127cf13787bab02e87
JezisVedToJeMojzis <mojzissamuel@gmail.com>
2023-01-30 15:21:07 +0100

    changed routes, fixed code, merged db.py and server.py (not tested)

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,83 +1,50 @@
 # #!flask/bin/python
-from flask import *
-# import os
-# from db import print_tasks_day, print_tasks_week, update_task, remove_task, create_task, init
-from db import init, create_task, update_task, remove_task
-# import sqlalchemy
-from dotenv import load_dotenv, find_dotenv
-import datetime
-
-
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+# from dotenv import load_dotenv, find_dotenv
 
 # load_dotenv(find_dotenv())
 
+db = SQLAlchemy()
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
-# db = sqlalchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+db.init_app(app)
+
+class Todo(db.Model):
+  __tablename__ = 'TodoTable'
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), unique=False, nullable=False)
+  description = db.Column(db.String(150), unique=False, nullable=False)
+
+  def __init__(self, title, description):
+    self.title = title
+    self.description = description
+
+db.create_all()
+
+@app.route('/server/todos', methods=['GET'])
+@cross_origin() 
+def get_todos(): 
+    todos = TodoTable.query.all()
+    return jsonify(todos)
 
 
+@app.route("/server/todos", methods=['POST'])
+@cross_origin()
+def create_todo_server():
+    body = request.get_json()
+    db.session.add(Todo(body['title'], body['description']))
+    db.session.commit()
+    return "todo successfully created"
 
-
-# class Item(db.Model):
-#     __tablename__='tasks'
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(80), unique=True, nullable=False)
-#     content = db.Column(db.String(120), unique=True, nullable=False)
-
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
-
-#
-@app.route("/")
-def initialize():
-    return init()
-
-# @app.route('/', methods=['POST'])
-# def my_form_post():
-#     taskID = request.form['taskID']
-#     taskDate = request.form['taskDate']
-#     startingTime = request.form['startingTime']
-#     endingTime = request.form['endingTime']
-#     # create_task(taskID, taskDate, startingTime, endingTime)
-#     return "<p> New task! Keep it up. </p>"
-# @app.route("/db/tasks/day", methods=['GET'])
-# def print_tasks_day():
-#     return jsonify({'tasks': get_day_tasks()})
-
-# @app.route("/db/tasks/week", methods=['GET'])
-# def print_tasks_week():
-#     return jsonify({'tasks': get_week_tasks()})
-
-@app.route("/server/createTask", methods=['GET','POST'])
-def create_task_serv():
-    date = datetime.date(2023,5,8)
-    timeStart = datetime.time(hour=20, minute=30)
-    timeEnd = datetime.time(hour=23, minute=30)
-    return create_task('papaya', date, timeStart, timeEnd)
-
-
-@app.route("/server/updateTask", methods=['GET'])
-def update_task_serv():
-#     input = request.get_json() #get input for the task
-#     taskID = input['taskID']
-#     taskDate = input['taskDate']
-#     startingTime = input['timeStart']
-#     endingTime = input['timeEnd']
-    date = datetime.date(2050,5,8)
-    timeStart = datetime.time(hour=20, minute=30)
-    timeEnd = datetime.time(hour=23, minute=30)
-    return update_task("papaya", "papaya_new", date, timeStart, timeEnd)
     
-
-@app.route("/server/removeTask", methods=['GET'])
-def remove_task_serv():
-    # input = request.get_json() #get input for the task
-    # taskID = input['taskID']
-    # taskDate = input['taskDate']
-    # startingTime = input['timeStart']
-    remove_task("papaya_new") #assume there cannot be 2 tasks with the same name in parallel
-    return "<p>Removed.</p>"
+@app.route("/server/todos/<id>", methods=['DELETE'])
+@cross_origin()
+def remove_todo_server(id):
+    db.session.query(Todo).filter_by(id=id).delete()
+    db.session.commit()
+    return "todo successfully deleted"
 
  
 if __name__ == '__main__': 
