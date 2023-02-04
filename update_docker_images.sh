852ae9778e686d934bb5ef5a6e8fc0acad8aa8cf
Paulo Liedtke <liedtke.paulo@gmail.com>
2023-02-04 15:13:31 +0100

    started to write helm charts

--- update_docker_images.sh
+++
@@ -1,5 +0,0 @@
-sudo docker build . -t localhost:32000/frontend:versionNumber frontend
-sudo docker push localhost:32000/frontend:versionNumber 
-
-sudo docker build . -t localhost:32000/backend:versionNumber
-sudo docker push localhost:32000/backend:versionNumber
